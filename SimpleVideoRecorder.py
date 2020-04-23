7ed0e55de1bcc7805a20e493760fe49b0bbeeba7
flanman1866 <flanman1866@gmail.com>
2020-04-22 18:04:56 -0600

    Added a filename updater and references to ip cameras for the video recorder.

--- SimpleVideoRecorder.py
+++ SimpleVideoRecorder.py
@@ -1,28 +1,37 @@
 import cv2
 import numpy as np
-import datetime
 import os
 
 
-camera = cv2.VideoCapture(0) 
-
+camera = cv2.VideoCapture(0) # this captures your default webcam
+##camera = cv2.VideoCapture('rtsp://192.168.1.64/1') # this is for an ip camera without a username & password
+##camera = cv2.VideoCapture('rtsp://username:password@192.168.1.64/1') # this would be ip cam with user & pass
 
 ## toggle to save or just display video
 SaveVideo = True
 ##SaveVideo = False
 
 
-Video_Title = "face"
+Video_Title = "GroundWatch_vid_"
+num = 0
+
+# updates file name so as not to overwrite videos
+for i in range(300):
+    title = Video_Title + str(num)+".avi"
+    if not os.path.exists(title):
+        break
+    else:
+        num += 1
+
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-title = Video_Title + ".avi"
 
 ret, frame = camera.read() 
 im_ht,im_wdt,im_chan = frame.shape
 dispWidth = im_wdt
 dispHeight = im_ht
 
-FPS = 30 #this should be experimented with
-VidOutput = cv2.VideoWriter(title,fourcc, FPS, (dispWidth,dispHeight))
+FPS = 20 #this should be experimented with
+VidOutput = cv2.VideoWriter(title, fourcc, FPS, (dispWidth,dispHeight))
 
 while True:
 
@@ -33,7 +42,6 @@ while True:
     if SaveVideo:
         VidOutput.write(frame) # save the frames
 
-   
     if cv2.waitKey(1) & 0xFF == ord('q'):  ## exit and close when user presses q key
         break
 
